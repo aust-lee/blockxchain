ed99191498caa0cd7d0b825e5fec62fb6fb23109
aust-lee <30392549+aust-lee@users.noreply.github.com>
2017-08-21 22:18:53 -0400

    Add files via upload

---
+++ frequency.py
@@ -0,0 +1,46 @@
+import sys
+import json
+import string
+import csv
+
+
+
+def main():
+      tweet_file = open(sys.argv[1])
+
+#sent_file = open('AFINN-111.txt', 'r')
+#tweet_file = open('data/stream_ETH.json', 'r')
+
+      counting = {}
+      total = 0
+      for line in tweet_file:
+          tweet = json.loads(line)
+          total_words = 0
+          count = 0
+          try:
+              for word in json.dumps(tweet["text"]).split():
+                  t_word = word.translate(None, string.punctuation).lower()
+                  total = total + 1
+                  if t_word in counting:
+                        counting[t_word] = counting[t_word] + 1
+                  else:
+                        counting[t_word] = 1
+                  
+                  
+                  
+                  
+
+          except:
+                total = total
+
+
+      totaling = {}
+      for word in counting:
+            totaling[word] = float(float(counting[word])/float(total))
+
+      for word in totaling:
+            print "{} {}".format(word, totaling[word])
+
+
+if __name__ == '__main__':
+      main()
